85171fb1cf5a72e8ea8a06e478aeb5311ce5dca6
shekhards2008 <70631825+shekhards2008@users.noreply.github.com>
2021-10-18 09:39:32 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,16 +1,29 @@
 
-class atm:
-    def __init__(self, card_number, pin_number):
-        self.card_number = card_number
-        self.pin_number = pin_number
+class Atm:
+    def __init__(self,card_number,pin):
+        self.card_number=card_number
+        self.pin=pin
+    def check_balance(self):
+        print('your balance is 100000')
+    def withdrawl(self,amount):
+        new_amount=100000-amount
+        print('you have withdrawl amount'+str(amount)+'.your remaining balance is '+str(new_amount))
 
+def main():
+    cardnumber=input('insert your card number')
+    pin_number=input('enter your pin number')
+    newuser=Atm(cardnumber,pin_number)
+    print('chose your activity')
+    print('1.balance enquiry  2.withdrawl')
+    activity=int(input('enter activity number'))
 
-    def cashWithdrawal(self, card_number):
-            print("Money has been successfully withdrawaled")
-    def balanceEnquiry(self, pin_number):
-            print("You have sufficient balance")
-    def cashDeposit(self, card_number):
-            print("Your cash is safely deposited")
+    if(activity==1):
+        newuser.check_balance()
+    elif(activity==2):
+        amount=int(input('enter the amount'))
+        newuser.withdrawl(amount)
+    else:
+        print('enter a valid number')
 
-idfcBank = atm(28382929929, 2020)
-print(idfcBank.cashWithdrawal(28382929929))
+if __name__=='__main__':
+    main()
